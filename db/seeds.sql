3b7a345537d42f37d5c132ad28578ca544240b6c
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-02-14 16:13:55 -0600

    Finished setting up initial DB setup.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
