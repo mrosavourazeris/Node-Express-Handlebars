7aa25dad5f23e3a899b539e18dc7d08e04edb886
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-03-11 05:49:03 -0600

    Updated seeds file.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);
